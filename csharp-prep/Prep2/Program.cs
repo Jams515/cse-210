e88c13f7a135590fb7dab41da841addff924634e
Jams515 <tay21050@byui.edu>
2024-01-13 18:49:21 -0700

    week01 and 02 prep work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percent do you have in this class? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a(n) {grade}!");
+
+        if (percent >= 70)
+        {
+            Console.Write("You passed!!");
+        }
+        else
+        {
+            Console.Write("You did not pass :( ");
+        }
+
+
     }
 }
